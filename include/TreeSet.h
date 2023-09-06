ad80e214ce8da652f53958160da79c47e4c699f8
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-09-06 10:48:15 +0200

    feat: Add TreeNode struct

--- include/TreeSet.h
+++ include/TreeSet.h
@@ -6,4 +6,13 @@ public:
     void insert(int value);
     void remove(int value);
     bool contains(int value);
+
+private:
+    struct TreeNode {
+        int data;
+        TreeNode *left;
+        TreeNode *right;
+        int height;
+    };
+    TreeNode *root;
 };
