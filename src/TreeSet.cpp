f79ed87c7ad872720c6544bd60da1f9a4a04e72d
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-09-06 10:51:35 +0200

    feat: Create implementation for AVL methods

--- src/TreeSet.cpp
+++ src/TreeSet.cpp
@@ -1,1 +1,21 @@
 #include "TreeSet.h"
+
+TreeSet::TreeSet() {
+
+}
+
+TreeSet::~TreeSet() {
+
+}
+
+void TreeSet::insert(int value) {
+
+}
+
+void TreeSet::remove(int value) {
+
+}
+
+bool TreeSet::contains(int value) {
+    return false;
+}
