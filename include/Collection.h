d1729939fb4cc946a7e81d6d398688ab8b81dd9b
Kieferer <balazsborsi0@gmail.com>
2023-09-09 22:29:16 +0200

    fix: Add 'value' integer parameter to 'contains' method of Collection

--- include/Collection.h
+++ include/Collection.h
@@ -2,6 +2,6 @@ class Collection {
 public:
     virtual void add(int value) = 0;
     virtual void remove(int value) = 0;
-    virtual bool contains() = 0;
+    virtual bool contains(int value) = 0;
     virtual int size() = 0;
 };
