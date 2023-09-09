f20fc299e8c4435c2e1afdcee0a6a2312299a3d5
Kieferer <balazsborsi0@gmail.com>
2023-09-09 22:54:44 +0200

    feat: Create setter methods for fields that can be changed

--- include/TreeSet/TreeNode.h
+++ include/TreeSet/TreeNode.h
@@ -2,12 +2,16 @@ class TreeNode {
 private:
     int key;
     int height;
-    TreeNode *left;
-    TreeNode *right;
+    TreeNode* left;
+    TreeNode* right;
 
 public:
     int getKey() const { return key; };
     int getHeight() const {return height; };
     TreeNode getLeft() const { return *left; };
     TreeNode getRight() const { return *right; };
+
+    void setHeight(int height) { this->height = height; }
+    void setLeft(TreeNode left) { this->left = &left; }
+    void setRight(TreeNode right) {this->right = &right; }
 };
