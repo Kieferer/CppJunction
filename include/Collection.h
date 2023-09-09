e5038ecc9d7a166677e3a0e9e3a65ec708c1365a
Kieferer <balazsborsi0@gmail.com>
2023-09-09 22:17:03 +0200

    fix: Add '= 0' to end of lines of method declarations to force programmer to implement the method

--- include/Collection.h
+++ include/Collection.h
@@ -1,7 +1,7 @@
 class Collection {
 public:
-    virtual void add(int value);
-    virtual void remove(int value);
-    virtual bool contains();
-    virtual int size();
+    virtual void add(int value) = 0;
+    virtual void remove(int value) = 0;
+    virtual bool contains() = 0;
+    virtual int size() = 0;
 };
