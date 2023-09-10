11aee334e337b19452916ab8392ee48e2285a4be
Kieferer <balazsborsi0@gmail.com>
2023-09-10 12:59:38 +0200

    refactor: Remove 'setHeight' setter method of 'TreeNode' because its never used since 'recalculateHeight' method updates the height

--- include/TreeSet/TreeNode.h
+++ include/TreeSet/TreeNode.h
@@ -20,7 +20,6 @@ public:
     TreeNode* getLeft() const { return left; };
     TreeNode* getRight() const { return right; };
 
-    void setHeight(int height) { this->height = height; }
     void setLeft(TreeNode* left) { this->left = left; }
     void setRight(TreeNode* right) { this->right = right; }
 
